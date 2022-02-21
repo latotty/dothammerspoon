a8b027aef2334e302d0c5a996223655f13fdef7c
LaTotty <latotty91@gmail.com>
2022-02-21 16:06:04 +0100

    Added screensaver hotkey on Hyper+Q

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ require('hs')
 require('layout')
 require('keyboard')
 require('imgur')
+require('screensaver')
 
 hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
---
+++ screensaver.lua
@@ -0,0 +1,1 @@
+hs.hotkey.bind(hyper, "q", nil, function() hs.timer.doAfter(1, function() hs.caffeinate.startScreensaver() end) end)
